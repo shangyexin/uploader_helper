3a08ea1b7c16f49f17588a1c57b8cfb8f2475f83
yasin <yexin.shang@gmail.com>
2022-06-15 18:32:55 +0800

    feat: 增加ui demo

--- main.py
+++ main.py
@@ -4,13 +4,22 @@
 # @Time   : 2022-06-15
 # @File   : main.py
 
+import sys
 import traceback
 
 from log import logger
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import media
 
 if __name__ == "__main__":
     try:
         logger.debug("Thank you for using uploader helper...")
+        app = QApplication(sys.argv)
+        myMainWindow = QMainWindow()
+        myUi = media.Ui_MainWindow()
+        myUi.setupUi(myMainWindow)
+        myMainWindow.show()
+        sys.exit(app.exec_())
     except Exception as e:
         logger.error(e)
         logger.error(traceback.format_exc())
